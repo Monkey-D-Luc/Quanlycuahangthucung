7b1fac00f757e92c719bdc72eedc4737b11d2db0
2155010104@kientruchanoi.edu.vn <2155010104@kientruchanoi.edu.vn>
2024-03-12 21:43:25 +0700

    test git

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -11,6 +11,7 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// sua
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
